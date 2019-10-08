46f4f6e4dc25690230551d2c9ddbe289389ad0a8
merlijnbrinks <merlijnbrinks@gmail.com>
2019-10-08 19:24:33 +0200

    Changes site

--- Nice_script.js
+++ Nice_script.js
@@ -1,36 +1,5 @@
-// JavaScript source code
-let niceQuote = Math.floor((Math.random() * 10) + 1);
-
-if (niceQuote === 1) {
-    document.getElementById("love").innerHTML = "Ik sta voor je klaar!";
-}
-else if (niceQuote === 2) {
-    document.getElementById("love").innerHTML = "Je bent echt een lieverd <3";
-}
-else if (niceQuote === 3) {
-    document.getElementById("love").innerHTML = "Je bent perfect precies zoals je bent xxx";
-}
-else if (niceQuote === 4) {
-    document.getElementById("love").innerHTML = "Dank je voor alles dat nog gaat komen xoxo";
-}
-else if (niceQuote === 5) {
-    document.getElementById("love").innerHTML = "Wij zijn zo mooi samen!";
-}
-else if (niceQuote === 6) {
-    document.getElementById("love").innerHTML = "Ik hou van je!!!!";
-}
-else if (niceQuote === 7) {
-    document.getElementById("love").innerHTML = "Ooit gaan we samen naar disney land Parijs x";
-}
-else if (niceQuote === 8) {
-    document.getElementById("love").innerHTML = "Je bent de beste";
-}
-else if (niceQuote === 9) {
-    document.getElementById("love").innerHTML = "Ik ben gelukkig <3";
-}
-else if (niceQuote === 10) {
-    document.getElementById("love").innerHTML = "Kusje!";
-}
-else {
-    document.getElementById("love").innerHTML = "ERROR #404 niceQuote value not found";
+let password = prompt("Password")
+if (password !== "Us4Ever"){
+    alert("Wrong password try again");
+    document.location.reload();
 }
